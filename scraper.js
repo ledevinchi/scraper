271a9436b8ae6957f8c854d7d3e02ff115dc231c
Rom Gavish <ledevinchi@gmail.com>
2020-11-01 09:25:51 +0200

    save

--- scraper.js
+++ scraper.js
@@ -11,10 +11,13 @@ exports.scrape = async () => {
 
     await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36");
 
-    var urls = await getpropertiesurls(page, '32205');
+    //var urls = await getpropertiesurls(page, '32205');
 
-    console.log('url: ', urls);
-    console.log('len: ', urls.length);
+    var tmpurl = 'https://www.zillow.com/homedetails/1840-Mallory-St-Jacksonville-FL-32205/44479106_zpid/';
+    await getpropertyjson(page, tmpurl);
+
+    //console.log('url: ', urls);
+    //console.log('len: ', urls.length);
     browser.close();
 }
 
@@ -24,8 +27,6 @@ async function getpropertiesurls(page, searchkey) {
     await page.waitForSelector('div.search-pagination ul li.PaginationNumberItem-c11n-8-10-0__bnmlxt-0 a', { visible: true });
     var urls = pageurls(page, [], 1, 1);
 
-
-    //const ul = await page.$$('div#grid-search-results ul li');
     return urls;
 }
 
@@ -72,4 +73,36 @@ async function pageurls(page, urls, pnumber, max) {
     var tmp = pnumber + 1;
     var urls = await pageurls(page, urls, tmp, max);
     return urls.concat(u);
+}
+
+async function getpropertyjson(page, url) {
+    console.log('url: ', url);
+    const sel = '#ds-container > div.ds-media-col.ds-media-col-hidden-mobile > ul > li > button > picture > img'
+    //docume      ("#ds-container > div.ds-media-col.ds-media-col-hidden-mobile > ul > li > button > picture > img")
+    await page.goto(url);
+    await page.waitForSelector(sel, { visible: true });
+    var images = await getimages(page);
+    console.log('images: ', images);
+    console.log('count: ', images.length);
+}
+
+async function getimages(page) {
+    const scrollable_section = '#ds-container > div.ds-media-col.ds-media-col-hidden-mobile > ul';
+
+    await page.evaluate(selector => {
+        const scrollableSection = document.querySelector(selector);
+
+        scrollableSection.scrollBottom = scrollableSection.offsetHeight;
+    }, scrollable_section);
+
+    await page.waitForSelector('#ds-container > div.ds-media-col.ds-media-col-hidden-mobile > ul > li figure');
+    
+    var imgs = await page.evaluate(() => {
+        const sel = '#ds-container > div.ds-media-col.ds-media-col-hidden-mobile > ul > li button picture';
+        var im = document.querySelectorAll(sel);
+        var arr = [];
+        im.forEach(e => arr.push(e.getAttribute('class')));
+        return arr;
+    });
+    return imgs;
 }
