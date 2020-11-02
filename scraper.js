1e733ac7994e162f6c81828a798225f5592c95ba
Rom Gavish <ledevinchi@gmail.com>
2020-11-02 23:52:32 +0200

    get property images
    get property images and the header data (price, address, sale status, size, bathrooms and bedrooms)

--- scraper.js
+++ scraper.js
@@ -2,18 +2,20 @@ const puppeteer = require('puppeteer');
 const UserAgent = require('user-agents');
 
 exports.scrape = async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({ headless: false, args: [`--window-size=1920,1080`], defaultViewport: null });
+    //args: [`--window-size=800,800`], defaultViewport: null
     //const context = await browser.createIncognitoBrowserContext();
     const page = await browser.newPage();
 
-    const firstUserAgent = new UserAgent();
-    const userAgent = firstUserAgent.random();
+
 
     await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36");
 
     //var urls = await getpropertiesurls(page, '32205');
 
-    var tmpurl = 'https://www.zillow.com/homedetails/1840-Mallory-St-Jacksonville-FL-32205/44479106_zpid/';
+    var tmpurl = 'https://www.zillow.com/homedetails/906-Greenridge-Rd-Jacksonville-FL-32207/44483311_zpid/'
+    //var tmpurl = 'https://www.zillow.com/homedetails/2973-Riverside-Ave-Jacksonville-FL-32205/44479421_zpid/';
+    //var tmpurl = "https://www.zillow.com/homedetails/3658-Walsh-St-Jacksonville-FL-32205/44489188_zpid/";
     await getpropertyjson(page, tmpurl);
 
     //console.log('url: ', urls);
@@ -77,31 +79,88 @@ async function pageurls(page, urls, pnumber, max) {
 
 async function getpropertyjson(page, url) {
     console.log('url: ', url);
-    const sel = '#ds-container > div.ds-media-col.ds-media-col-hidden-mobile > ul > li > button > picture > img'
-    //docume      ("#ds-container > div.ds-media-col.ds-media-col-hidden-mobile > ul > li > button > picture > img")
-    await page.goto(url);
-    await page.waitForSelector(sel, { visible: true });
-    var images = await getimages(page);
-    console.log('images: ', images);
-    console.log('count: ', images.length);
+    await page.goto(url, { waitUntil: 'networkidle0', });
+
+    // ==== get images ====
+    // const sel = '#ds-container > div.ds-media-col.ds-media-col-hidden-mobile > ul li img'
+    // await page.waitForSelector(sel, { visible: true });
+    // var images = await getimages(page);
+
+    // ==== get badrooms, bathrooms, size, address ====
+    const bb = await getheader(page);
+
+}
+
+async function getheader(page) {
+    //badrooms
+    const res = await page.evaluate(() => {
+        const bd = document.querySelector('#ds-container header.ds-bed-bath-living-area-header > h3 > span:nth-child(1) > span:nth-child(1)').innerText;
+        const ba = document.querySelector('#ds-container header.ds-bed-bath-living-area-header > h3 > button > span > span:nth-child(1)').innerText;
+        const sqf = document.querySelector('#ds-container header.ds-bed-bath-living-area-header > h3 > span:nth-child(5) > span:nth-child(1)').innerText;
+
+        const price = document.querySelector('#ds-container div.ds-summary-row-container div h3 span.ds-value').innerText.replace(/[^0-9]+/g, "");
+
+        const salestatus = document.querySelector('#ds-container > div.ds-data-col.ds-white-bg.ds-data-col-data-forward > div.ds-chip > div > div.sc-oUcyK.lbUOdM.ds-chip-removable-content > p > span.sc-pzMyG.ijxQnZ.ds-status-details').innerText;
+
+        const add1 = document.querySelector('#ds-container header > h1.ds-address-container > span:nth-child(1)').innerText;
+        const add2 = document.querySelector('#ds-container header > h1.ds-address-container > span:nth-child(2)').innerText;
+
+        return {
+            badrooms: parseInt(bd),
+            bathrooms: parseInt(ba),
+            size: parseInt(sqf.replace(',', '')),
+            price: parseInt(price),
+            salestatus: salestatus,
+            address: {
+                add1: add1,
+                add2: add2
+            }
+        }
+    });
+
+    let citystatezip = res.address.add2.split(',');
+    let statezip = citystatezip[1].trim().split(' ');
+
+    var address = {
+        street: res.address.add1.replace(',', '').trim(),
+        city: citystatezip[0].trim(),
+        state: statezip[0].trim(),
+        zipcode: statezip[1].trim()
+    }
+    res.address = address;
+    console.log(res);
+    return res;
 }
 
 async function getimages(page) {
-    const scrollable_section = '#ds-container > div.ds-media-col.ds-media-col-hidden-mobile > ul';
 
-    await page.evaluate(selector => {
-        const scrollableSection = document.querySelector(selector);
+    const lilen = await page.evaluate(() => {
+        const selq = '#ds-container > div.ds-media-col.ds-media-col-hidden-mobile ul li';
+        let lis = document.querySelectorAll(selq);
 
-        scrollableSection.scrollBottom = scrollableSection.offsetHeight;
-    }, scrollable_section);
+        for (let i = 0; i < lis.length; i++) {
+            const el = lis[i];
+            setTimeout(() => {
+                el.scrollIntoView({ behavior: "smooth", block: "end", inline: "end" });
+            }, 10);
+        }
+        const selector = '#ds-container > div.ds-media-col.ds-media-col-hidden-mobile';
+        const elem = document.querySelector(selector);
+        elem.scrollTop = elem.scrollHeight;
 
-    await page.waitForSelector('#ds-container > div.ds-media-col.ds-media-col-hidden-mobile > ul > li figure');
-    
-    var imgs = await page.evaluate(() => {
-        const sel = '#ds-container > div.ds-media-col.ds-media-col-hidden-mobile > ul > li button picture';
+        return elem.scrollHeight;
+    });
+
+    await page.waitForSelector('#ds-container > div.ds-media-col.ds-media-col-hidden-mobile > ul li figure');
+
+    const imgs = await page.evaluate(() => {
+        const sel = '#ds-container > div.ds-media-col.ds-media-col-hidden-mobile > ul > li button img';
         var im = document.querySelectorAll(sel);
         var arr = [];
-        im.forEach(e => arr.push(e.getAttribute('class')));
+        im.forEach(e => {
+            e.scrollIntoView();
+            arr.push(e.getAttribute('src'));
+        });
         return arr;
     });
     return imgs;
